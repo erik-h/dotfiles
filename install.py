5d727235fb7c8bd3eb3307043f76b4f3a92865de
Erik H <youdulayo@gmail.com>
2018-01-14 21:16:24 -0700

    Add touchpad config file and TODO for installing it with my installer in /etc/

--- install.py
+++ install.py
@@ -5,6 +5,9 @@
 # NOTE: This scripts requires Python3.
 #
 
+# TODO: use `stow -t /etc etc` style and add another mode to this installer
+# that works with system-wide files stowed relative to root.
+
 from glob import glob
 import subprocess
 import sys
@@ -12,7 +15,7 @@ import shutil
 import os
 
 # Put any non-dotfile containing directories to ignore in this list.
-EXCLUDE = ["./PACKAGES/"]
+EXCLUDE = ["PACKAGES"]
 
 # Ensure GNU Stow is installed, as it is the program we use to install our dotfiles
 if shutil.which("stow") is None:
@@ -21,9 +24,11 @@ if shutil.which("stow") is None:
 
 # Each directory alongside this script contains dotfiles
 for target in glob("./*/"):
+    # We use normpath because Stow wants just the directory name with no
+    # leading "./" or trailing "/".
+    target = os.path.normpath(target)
     if target not in EXCLUDE:
         print("Installing", target)
-        # We use normpath because Stow wants just the directory name with no
-        # leading "./" or trailing "/".
-        subprocess.call(["stow", os.path.normpath(target)])
+        subprocess.call(["stow", target])
+
 print("Done!")
