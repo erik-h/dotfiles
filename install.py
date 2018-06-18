6d561a209e68a7ac3ac1928bfc65fbe233898299
Erik H <public+git@erikh.me>
2018-06-18 16:37:17 -0600

    Wrap installer in function to allow catching of SIGINT

--- install.py
+++ install.py
@@ -23,45 +23,53 @@ import os
 EXCLUDE = ["PACKAGES", "etc", "userChrome.css", "userContent.css"]
 FIREFOX_DIR = os.path.expanduser("~/.mozilla/firefox")
 FIREFOX_PROFILES_INI = os.path.join(FIREFOX_DIR, "profiles.ini")
-if not os.path.exists(FIREFOX_PROFILES_INI):
-    print("[WARN] Firefox profiles.ini doesn't exist at '{}'; skipping userChrome.css install...".format(FIREFOX_PROFILES_INI))
-else:
-    config = configparser.ConfigParser()
-    config.read(FIREFOX_PROFILES_INI)
 
-    valid_names = []
-    print("Firefox profile names:")
-    for i, section in enumerate(config.sections()):
-        for key, value in config.items(section):
-            if key == "name":
-                print("{} -> {}".format(i, value))
-                valid_names.append(section)
+def main():
+    if not os.path.exists(FIREFOX_PROFILES_INI):
+        print("[WARN] Firefox profiles.ini doesn't exist at '{}'; skipping userChrome.css install...".format(FIREFOX_PROFILES_INI))
+    else:
+        config = configparser.ConfigParser()
+        config.read(FIREFOX_PROFILES_INI)
 
-    profile_index = -1
-    while profile_index not in range(1, len(valid_names)+1):
-        profile_index = int(input("In which Firefox profile would you like to install 'userChrome.css' and 'userContent.css'?: "))
+        valid_names = []
+        print("Firefox profile names:")
+        for i, section in enumerate(config.sections()):
+            for key, value in config.items(section):
+                if key == "name":
+                    print("{} -> {}".format(i, value))
+                    valid_names.append(section)
 
-    chosen_profile = valid_names[profile_index-1]
-    print("Chose:", config[chosen_profile]["name"])
-    chosen_profile_path = os.path.join(FIREFOX_DIR, config[chosen_profile]["path"])
-    print("\tProfile path:", chosen_profile_path)
-    # TODO: At this point we have the directory of their profile.
-    # We should then:
-    # - `mkdir -p` $THEDIRECTORY/chrome
-    # - symlink the userChrome.css there
+        profile_index = -1
+        while profile_index not in range(1, len(valid_names)+1):
+            profile_index = int(input("In which Firefox profile would you like to install 'userChrome.css' and 'userContent.css'?: "))
+
+        chosen_profile = valid_names[profile_index-1]
+        print("Chose:", config[chosen_profile]["name"])
+        chosen_profile_path = os.path.join(FIREFOX_DIR, config[chosen_profile]["path"])
+        print("\tProfile path:", chosen_profile_path)
+        # TODO: At this point we have the directory of their profile.
+        # We should then:
+        # - `mkdir -p` $THEDIRECTORY/chrome
+        # - symlink the userChrome.css there
 
 # Ensure GNU Stow is installed, as it is the program we use to install our dotfiles
-if shutil.which("stow") is None:
-    print("[ERROR] GNU Stow is required to install these dotfiles (https://www.gnu.org/software/stow/).", file=sys.stderr)
-    sys.exit(1)
+    if shutil.which("stow") is None:
+        print("[ERROR] GNU Stow is required to install these dotfiles (https://www.gnu.org/software/stow/).", file=sys.stderr)
+        sys.exit(1)
 
 # Each directory alongside this script contains dotfiles
-for target in glob("./*/"):
-    # We use normpath because Stow wants just the directory name with no
-    # leading "./" or trailing "/".
-    target = os.path.normpath(target)
-    if target not in EXCLUDE:
-        print("Installing", target)
-        subprocess.call(["stow", target])
+    for target in glob("./*/"):
+        # We use normpath because Stow wants just the directory name with no
+        # leading "./" or trailing "/".
+        target = os.path.normpath(target)
+        if target not in EXCLUDE:
+            print("Installing", target)
+            subprocess.call(["stow", target])
 
-print("Done!")
+    print("Done!")
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\nCaught SIGINT; stopping...")
