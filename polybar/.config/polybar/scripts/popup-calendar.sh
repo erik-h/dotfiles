4773cc34067a7c18f098e150e58698937a056f07
Erik Haugrud <public+git@erikh.me>
2022-04-01 15:41:24 -0600

    polybar: add pop-up calendar

---
+++ polybar/.config/polybar/scripts/popup-calendar.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+# Original source: https://github.com/polybar/polybar-scripts/tree/0110d7aa915cc22bf3f596f37c0e6af4d1c7bce5/polybar-scripts/popup-calendar
+
+BAR_HEIGHT=36  # polybar height
+BORDER_SIZE=1  # border size from your wm settings
+YAD_WIDTH=222  # 222 is minimum possible value
+YAD_HEIGHT=193 # 193 is minimum possible value
+DATE="$(date +"%a %d%l:%M %p")"
+
+case "$1" in
+--popup)
+    if [ "$(xdotool getwindowfocus getwindowname)" = "yad-calendar" ]; then
+        exit 0
+    fi
+
+    eval "$(xdotool getmouselocation --shell)"
+    eval "$(xdotool getdisplaygeometry --shell)"
+
+    # X
+    if [ "$((X + YAD_WIDTH / 2 + BORDER_SIZE))" -gt "$WIDTH" ]; then #Right side
+        : $((pos_x = WIDTH - YAD_WIDTH - BORDER_SIZE))
+    elif [ "$((X - YAD_WIDTH / 2 - BORDER_SIZE))" -lt 0 ]; then #Left side
+        : $((pos_x = BORDER_SIZE))
+    else #Center
+        : $((pos_x = X - YAD_WIDTH / 2))
+    fi
+
+    # Y
+    if [ "$Y" -gt "$((HEIGHT / 2))" ]; then #Bottom
+        : $((pos_y = HEIGHT - YAD_HEIGHT - BAR_HEIGHT - BORDER_SIZE))
+    else #Top
+        : $((pos_y = BAR_HEIGHT + BORDER_SIZE))
+    fi
+
+    yad --calendar --undecorated --fixed --close-on-unfocus --no-buttons \
+        --width="$YAD_WIDTH" --height="$YAD_HEIGHT" --posx="$pos_x" --posy="$pos_y" \
+        --title="yad-calendar" --borders=0 >/dev/null &
+    ;;
+*)
+    echo "$DATE"
+    ;;
+esac
