c19a7a9739ad7f6f612d29f90a42fbd56f2e336c
Erik Haugrud <public+git@erikh.me>
2022-04-07 23:18:18 -0600

    polybar: replace Spotify-specific status with generic "media" status script, add pomodoro script

---
+++ polybar/.config/polybar/scripts/media_status.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# playerctl metadata | grep ':title' | tr -s ' ' | cut -d' ' -f3-
+
+MEDIA_STATUS_MAX_LEN=40
+
+metadata="$(playerctl metadata 2>&1)"
+if [[ "$metadata" == "No players found" ]]; then
+	# We've got no media playing
+	exit 0
+fi
+
+title="$(grep ':title' <<< "$metadata" | tr -s ' ' | cut -d' ' -f3-)"
+artist="$(grep ':artist' <<< "$metadata" | tr -s ' ' | cut -d' ' -f3-)"
+player_status="$(playerctl status)"
+if [[ "$player_status" == "Playing" ]]; then
+	media_msg=" $title - $artist"
+else
+	media_msg=" $title - $artist"
+fi
+if [[ $(wc -c <<< "$media_msg") -gt $MEDIA_STATUS_MAX_LEN ]]; then
+	echo "${media_msg:0:$MEDIA_STATUS_MAX_LEN}..."
+else
+	echo "$media_msg"
+fi
