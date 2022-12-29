5bf62949e95dc17957ebe66e95cba574b18f159a
Erik Haugrud <public+git@erikh.me>
2022-12-29 01:58:19 -0700

    nvim: move colorscheme config below plugin initialization
    Since the colorscheme is installed as a plugin, it must be set *after* we've
    initialized the plugins.

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -161,11 +161,12 @@ table.insert(vim.opt.wildignore, "*/node_modules/*")
 vim.opt.ttimeoutlen = 0
 vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]])
 
+
+require("plugins")
+
 --
 -- Colorscheme
 --
 -- TODO: not sure if I need a dedicated section/file for this... maybe
 -- ageneric "colors.lua"?
 vim.cmd("colorscheme dracula")
-
-require("plugins")
