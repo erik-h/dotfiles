40fb66bf6199092b4ca958b05bfd22a355193616
Erik Haugrud <erik@Eriks-MBP.lan>
2023-11-05 17:37:39 -0700

    wezterm: use proper fish path on macOS

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,10 +1,17 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
+function file_exists(name)
+	return next(wezterm.glob(name)) ~= nil
+end
+
 config.color_scheme = 'Dracula'
 
--- Use fish and tmux
-config.default_prog = { '/usr/bin/fish', '-l', '-c', 'tmux' }
+if file_exists('/opt/homebrew/bin/fish') then
+	config.default_prog = { '/opt/homebrew/bin/fish', '-l', '-c', '/opt/homebrew/bin/tmux' }
+elseif file_exists('/usr/bin/fish') then
+	config.default_prog = { '/usr/bin/fish', '-l', '-c', '/usr/bin/tmux' }
+end
 
 -- Don't use the tab bar; I'll use tmux windows
 config.enable_tab_bar = false
