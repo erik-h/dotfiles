ef318182cb56a978ce5d64723624a460482430c7
Erik Haugrud <public+git@erikh.me>
2024-06-07 21:20:09 -0600

    fish: don't use minikube's `kubectl`

--- fish/.config/fish/functions/kubectl.fish
+++ fish/.config/fish/functions/kubectl.fish
@@ -1,3 +1,8 @@
 function kubectl --description 'Run kubectl using minikube'
-	minikube kubectl -- $argv
+	# Use kubernetes-api from brew's kubectl instead of minikube.
+	# The minikube version doesn't respect ~/.kube/config apparently... it was
+	# ignoring my config setup for EKS.
+	command kubectl $argv
+	# minikube kubectl -- $argv
+	# minikube -p oauth2-proxy kubectl -- $argv
 end
