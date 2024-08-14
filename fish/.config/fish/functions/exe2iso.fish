42e59ffb6180184e9111adbc2362645b28b1029a
Erik Haugrud <public+git@erikh.me>
2024-08-14 17:29:47 -0600

    fish: add usage message to exe2iso function

--- fish/.config/fish/functions/exe2iso.fish
+++ fish/.config/fish/functions/exe2iso.fish
@@ -1,5 +1,9 @@
 # Usage: exe2iso <existing exe file> <new iso file>
 function exe2iso --description "Extract an exe and plop its contents into an iso"
+	if test (count $argv) -lt 2
+		echo "Usage:" (status function) "<existing exe file> <new iso file>" >&2
+		return 1
+	end
 	set -l extracted_dir $argv[1]_extracted
 	7z x -o$extracted_dir $argv[1]
 	hdiutil makehybrid -iso -joliet -o $argv[2] $extracted_dir
