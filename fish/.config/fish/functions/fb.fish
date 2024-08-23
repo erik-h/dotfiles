f77afbf5cc06facd068f9604dd0d759c93c4aefe
Erik Haugrud <public+git@erikh.me>
2024-08-22 18:30:35 -0600

    fish: rename my new `fb` fuzzy branch switcher function to my `fgco`
    It now takes the place of "fuzzy git checkout" aka `fgco` but that's the
    one in my muscle memory.

--- fish/.config/fish/functions/fb.fish
+++
@@ -1,6 +0,0 @@
-function fb
-	if git rev-parse HEAD >/dev/null 2>&1
-		set branch (bash -c "source $XDG_CONFIG_HOME/fzf/fzf-git.sh && _fzf_git_branches")
-		git switch $branch
-	end
-end
--- fish/.config/fish/functions/fgco.fish
+++ fish/.config/fish/functions/fgco.fish
@@ -1,8 +1,10 @@
 function fgco
-	set -l branch (_fuzzy_git_branch $argv[1])
-	if [ -z "$branch" ]
-		return 1
-	end
+	if git rev-parse HEAD >/dev/null 2>&1
+		set branch (bash -c "source $XDG_CONFIG_HOME/fzf/fzf-git.sh && _fzf_git_branches")
+		if [ -z "$branch" ]
+			return 1
+		end
 
-	git checkout $branch
+		git switch $branch
+	end
 end
