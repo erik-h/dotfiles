fb09e5d42e14787510148a1a0dbe350278ab7926
Erik Haugrud <public+git@erikh.me>
2025-01-28 20:18:18 -0700

    fish: add go-template version of kubesecret, commented out
    I can't remember why I switched so I'll just leave the commented out
    go-template version there lol.

--- fish/.config/fish/functions/kubesecret.fish
+++ fish/.config/fish/functions/kubesecret.fish
@@ -1,3 +1,6 @@
 function kubesecret --description 'Decode and display a k8s secret base64 string values'
 	kubectl get secret $argv[1] -o json | jq '.data | map_values(@base64d)'
+	# NOTE: I switched to the go-template version before at some point, but I don't rememberw why...
+	# I think I had encountered some situation where it was better? Hmm.
+	#kubectl get secret $argv[1] -o go-template='{{range $k,$v := .data}}{{printf "%s: " $k}}{{if not $v}}{{$v}}{{else}}{{$v | base64decode}}{{end}}{{"\n"}}{{end}}'
 end
