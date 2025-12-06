30531db5c5b35652d0a1bf9fff22b3e8324071be
Erik Haugrud <public+git@erikh.me>
2025-12-06 04:29:59 -0700

    nvim: remove org from treesitter config
    Apparently it doesn't exist anymore or was renamed (?). I can figure it out
    later since I'm mostly just using norg or markdown nowadays.

--- neovim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ neovim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,11 +1,29 @@
 require("nvim-treesitter.configs").setup {
 	ensure_installed = {
-		"c", "c_sharp", "lua", "rust", "go", "http", "python", "java",
-		"kotlin", "javascript", "bash", "org", "json", "ruby", "hcl",
-		"terraform", "tsx", "vim", "yaml", "html", "xml"
+		"c",
+		"c_sharp",
+		"lua",
+		"rust",
+		"go",
+		"http",
+		"python",
+		"java",
+		"kotlin",
+		"javascript",
+		"bash",
+		-- "org",
+		"json",
+		"ruby",
+		"hcl",
+		"terraform",
+		"tsx",
+		"vim",
+		"yaml",
+		"html",
+		"xml"
 	},
 	highlight = {
 		enable = true,
-		additional_vim_regex_highlighting = {"org"}, -- Required for spellcheck, some LaTex highlights and code block highlights that do not have ts grammar
+		-- additional_vim_regex_highlighting = {"org"}, -- Required for spellcheck, some LaTex highlights and code block highlights that do not have ts grammar
 	},
 }
