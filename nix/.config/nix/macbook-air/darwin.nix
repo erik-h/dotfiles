bf8090ce35bf33f090c65c839d723a60c0aca27c
Erik H <public@erikh.dev>
2026-03-26 17:07:41 -0600

    nix: add initial nix config for macbook-air

---
+++ nix/.config/nix/macbook-air/darwin.nix
@@ -0,0 +1,44 @@
+{ self, pkgs, ...}:
+
+{
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages = with pkgs; [
+        vim
+	neovim
+      ];
+
+      users.users.erik = {
+            name = "erik";
+            home = "/Users/erik";
+      };
+      
+      ids.gids.nixbld = 350;
+      
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+      
+      # Enable alternative shell support in nix-darwin.
+      programs.fish.enable = true;
+
+      homebrew = {
+      	enable = true;
+	casks = [
+		"zen-browser"
+	];
+	onActivation.cleanup = "zap";
+      };
+
+      security.pam.services.sudo_local.touchIdAuth = true;
+      
+      # Set Git commit hash for darwin-version.
+      # system.configurationRevision = self.rev or self.dirtyRev or null;
+      
+      system.primaryUser = "erik";
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 6;
+      
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+}
